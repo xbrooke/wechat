cd8bf70e5e405344f63394cb7a1652f09de07ea5
Your Name <554719247@qq.com>
2024-07-23 23:08:05 +0800

    更新评论

---
+++ config.js
@@ -0,0 +1,15 @@
+const oa_name = "徐大兵";
+const oa_description = "分享设计与科技生活";
+
+function replaceText(elementId, newText) {
+  var element = document.getElementById(elementId);
+  if (element) {
+      element.innerText = newText;
+  } else {
+      console.log("Element with ID '" + elementId + "' not found.");
+  }
+}
+
+replaceText('wechatOA-name', oa_name);
+replaceText('wechatOA-description', oa_description)
+replaceText('web-beian', web_beian)
